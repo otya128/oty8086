e2e34b819ea6f65d72bdf3480ac6a24c7dae8342
otya128 <otya128@users.noreply.github.com>
2014-06-03 19:39:07 +0900

    init

---
+++ src/com/github/otya128/Machine8086.java
@@ -0,0 +1,9 @@
+package com.github.otya128;
+
+public class Machine8086 {
+    byte[] Memory;
+    public Machine8086()
+    {
+
+    }
+}
---
+++ src/com/github/otya128/Main.java
@@ -0,0 +1,10 @@
+package com.github.otya128;
+import java.lang.System;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("aho");
+
+    }
+}
